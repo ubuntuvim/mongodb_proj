1da9ddd89c7a4adbc235e59fffef5dfe30ff3992
ubuntuvim <1527254027@qq.com>
2016-05-06 12:47:40 +0800

    初步完成注册

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var session = require('express-session');
- 
+var mongoose = require('mongoose');
+
 var app = express();
 // app.get('/', function(req, res) {
 // 	res.render('register');  //跳转到注册页面的路由
@@ -22,6 +23,27 @@ app.use(express.static(require('path').join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
+
+// 连接数据库
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+app.use(session({
+  secret: 'secret',
+  cookie: {
+    maxAge: 1000*60*30
+  }
+}));
+
+// 在register的post请求处理中我们使用了express-session模块来保存相关信息，
+// 这里我们就使用中间件来传递这些提示信息
+app.use(function(req, res, next) {
+  res.locals.user = req.session.user;  //保存用户信息
+  var err = req.session.error;  //保存错误的结果响应信息
+  res.locals.message = '';
+  if (err)
+    res.locals.message = '<div class="alert alert-danger" style="margin-bottom: 20px; color: red;"' + err + '</div>';
+
+  next();
+});
 
 //  引入路由模块
 require('./routes')(app);
--- common/models.js
+++ common/models.js
@@ -7,5 +7,24 @@ module.exports = {
    	name: { type: String, required: true },
      	password: { type: String, required: true },
      	gender: { type: Boolean, default: true }
+   },
+   //  商品
+  //  name(商品名称)、price(商品价格)、imgSrc(商品展示图片路径)
+   commodity: {
+     name: { type: String },
+     price: { type: Number },
+     imgSrc: { type: String }
+   },
+   // 购物车
+  //  uId(用户ID)、cId(商品ID)、cName(商品名称)、cPrice(商品价格)、
+  // cImgSrc(商品展示图片路径)、cQuantity(商品数量)、cStatus(商品结算状态，未结算为false,已结算为true)
+   carts: {
+     uId: { type: String },
+     cId: { type: String },
+     cName: { type: String },
+     cPrice: { type: String },
+     cImgId: { type: String },
+     cQuantity: { type: String },
+     cStatus: { type: Boolean, default: false }
    }
 };
